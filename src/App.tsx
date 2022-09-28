7d5d2e5d44919a2fa2a3628cd5315f97a3dabc3e
diegosmoreira <soares.diego95@hotmail.com>
2022-09-28 07:00:11 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Header } from './components/Header';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <div>Hello World!</div>
+    </div>
+  )
+}
+
+export default App
